4827209d99dfc9c00ec6456b2af8dbbecf129fe4
Gerrald <gerrald@weelden.net>
2026-06-05 10:24:48 +0200

    Added comments

--- applications/app_custom_debug.c
+++ applications/app_custom_debug.c
@@ -1,9 +1,9 @@
 /*
-	Copyright 2019 Benjamin Vedder	benjamin@vedder.se
+    Copyright 2019 Benjamin Vedder	benjamin@vedder.se
 
-	This file is part of the VESC firmware.
+    This file is part of the VESC firmware.
 
-	The VESC firmware is free software: you can redistribute it and/or modify
+    The VESC firmware is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -15,6 +15,8 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Custom app to debug the internal state of the parking brake
     */
 
 #include "app.h"
@@ -37,62 +39,65 @@
 #include <string.h>
 #include <stdio.h>
 
-
 // Private functions
 static void terminal_debug(int argc, const char **argv);
 
-// Called when the custom application is started. Start our
-// threads here and set up callbacks.
-void app_custom_start(void) {
-	// Terminal commands for the VESC Tool terminal can be registered.
-	terminal_register_command_callback(
-			"print_debug_parking",
-			"Print debug info about parking",
-			"[d]",
-			terminal_debug);
+/**
+ * Called when the application is started
+ * Set a callback
+ */
+void app_custom_start(void)
+{
+    // Terminal commands for the VESC Tool terminal can be registered.
+    terminal_register_command_callback(
+        "print_debug_parking",
+        "Print debug info about parking",
+        "[d]",
+        terminal_debug);
 }
 
-// Called when the custom application is stopped. Stop our threads
-// and release callbacks.
-void app_custom_stop(void) {
-	terminal_unregister_callback(terminal_debug);
+/**
+ * Called when the application is stopped
+ * Release callback.
+ */
+void app_custom_stop(void)
+{
+    terminal_unregister_callback(terminal_debug);
 }
 
-void app_custom_configure(app_configuration *conf) {
-	(void)conf;
+/**
+ * Set the app configuration.
+ *
+ * @param conf the app configuration
+ */
+void app_custom_configure(app_configuration *conf)
+{
+    (void)conf;
 }
 
+/**
+ * Callback function for the terminal. Used to debug the internal state of the parking brake
+ */
+static void terminal_debug(int argc, const char **argv)
+{
 
-// Callback function for the terminal command with arguments.
-static void terminal_debug(int argc, const char **argv) {
-	// if (argc == 2) {
-	// 	int d = -1;
-	// 	sscanf(argv[1], "%d", &d);
+    // Get output
+    bool is_enabled = mcpwm_dc_is_parking_brake_enabled();
+    bool is_engaged = mcpwm_dc_is_parking_brake_engaged();
+    float duty = mcpwm_dc_get_parking_brake_duty();
+    uint16_t ccr_call_n = mcpwm_dc_get_ccr_called();
+    uint32_t reg_CCR2 = mcpwm_reg_get_CCR2();
+    uint32_t reg_ARR = mcpwm_reg_get_ARR();
+    uint32_t reg_CCER = mcpwm_reg_get_CCER();
+    uint32_t reg_CCMR1 = mcpwm_reg_get_CCMR1();
+    uint32_t reg_BDTR = mcpwm_reg_get_BDTR();
 
-	// 	commands_printf("You have entered %d", d);
-
-	// 	// For example, read the ADC inputs on the COMM header.
-	// 	commands_printf("ADC1: %.2f V ADC2: %.2f V",
-	// 			(double)ADC_VOLTS(ADC_IND_EXT), (double)ADC_VOLTS(ADC_IND_EXT2));
-	// } else {
-	// 	commands_printf("This command requires one argument.\n");
-	// }
-	(void *) argc;
-	(void *) argv;
-	bool is_enabled = mcpwm_dc_is_parking_brake_enabled();
-	bool is_engaged = mcpwm_dc_is_parking_brake_engaged();
-	float duty = mcpwm_dc_get_parking_brake_duty();
-	uint16_t ccr_call_n = mcpwm_dc_get_ccr_called();
-	uint32_t reg_CCR2 = mcpwm_reg_get_CCR2();
-	uint32_t reg_ARR = mcpwm_reg_get_ARR();
-	uint32_t reg_CCER = mcpwm_reg_get_CCER();
-	uint32_t reg_CCMR1 = mcpwm_reg_get_CCMR1();
-	uint32_t reg_BDTR = mcpwm_reg_get_BDTR();
-	commands_printf("PB enabled=%d engaged=%d duty=%.3f called=%d",
-					is_enabled, is_engaged, (double)duty, ccr_call_n);
-	commands_printf("	reg_CCR2=%d", reg_CCR2);
-	commands_printf("	reg_ARR=%d", reg_ARR);
-	commands_printf("	reg_CCER=%d", reg_CCER);
-	commands_printf("	reg_CCMR1=%d", reg_CCMR1);
-	commands_printf("	reg_BDTR=%d", reg_BDTR);
+    // Print it
+    commands_printf("PB enabled=%d engaged=%d duty=%.3f called=%d",
+                    is_enabled, is_engaged, (double)duty, ccr_call_n);
+    commands_printf("	reg_CCR2=%d", reg_CCR2);
+    commands_printf("	reg_ARR=%d", reg_ARR);
+    commands_printf("	reg_CCER=%d", reg_CCER);
+    commands_printf("	reg_CCMR1=%d", reg_CCMR1);
+    commands_printf("	reg_BDTR=%d", reg_BDTR);
 }
--- motor/mcpwm_dc_parking_brake.c
+++ motor/mcpwm_dc_parking_brake.c
@@ -1,3 +1,8 @@
+/**
+ * Contains all functions that have to do with the parking brake to seperate the parking brake from the normal driving logic.
+ * These functions are called from mcpwm_dc.c so they will only run if the motor type is DC.
+ */
+
 #include "ch.h"
 #include "hal.h"
 #include "stm32f4xx_conf.h"
@@ -25,8 +30,14 @@ static volatile bool parking_brake_output_engaged;
 
 static volatile uint16_t is_ccr_called;
 
-static void apply_parking_brake(float dutycycle, bool engaged);
+static void apply_parking_brake(float dutycycle, bool output_engaged);
 
+/**
+ * Initializes the parking brake driver.
+ * The pins and timer are initialized in mcpwm_dc.c because some things can be reused
+ * 
+ * @param mc_configuration The motor config
+ */
 void mcpwm_dc_parking_brake_init(volatile mc_configuration *configuration)
 {
     utils_sys_lock_cnt();
@@ -38,12 +49,18 @@ void mcpwm_dc_parking_brake_init(volatile mc_configuration *configuration)
     is_ccr_called = 0;
 }
 
+/**
+ * Updates the configuration
+ * 
+ * @param mc_configuration The motor config
+ */
 void mcpwm_dc_parking_brake_set_configuration(volatile mc_configuration *configuration)
 {
     // Stop everything first to be safe
     dutycycle_parking_brake_now = 0;
     parking_brake_output_engaged = false;
     mcpwm_dc_parking_brake_stop_pwm();
+
     utils_sys_lock_cnt();
     conf = configuration;
     utils_sys_unlock_cnt();
@@ -51,25 +68,24 @@ void mcpwm_dc_parking_brake_set_configuration(volatile mc_configuration *configu
 
 /**
  * The parking brake control loop
- *
+ * Should be called from mcpwm_dc_adc_int_handler in mcpwm_dc.c
+ * Now only sets a fixed dutycycle. This should be changed to a PID controller for current.
  */
 void update_duty_cycle_parking_brake(void)
 {
-    float dutycycle = 0.5f;
+    float dutycycle = parking_brake_output_engaged ? .5f : 0.0f;
     dutycycle_parking_brake_now = dutycycle;
     // Apply
     apply_parking_brake(dutycycle, parking_brake_output_engaged);
 }
 
 /**
- * Applies the dutycycle and state of the parking brake
+ * Applies the dutycycle and state of the parking brake if configured so in config.
  *
  * @param dutycycle - the dutycycle to apply
- * @param engaged - If engaged is true, make sure the output is off.
- * 		This is because the wheelchair needs current to switch off the parking brake (NC)
- * NOTE: I made now that if engaged true, output on. Makes for less mistakes
+ * @param output_engaged - If engaged is true, make sure the parking brake output is on.
  */
-static void apply_parking_brake(float dutycycle, bool engaged)
+static void apply_parking_brake(float dutycycle, bool output_engaged)
 {
     // Only run if parking brake is enabled in config
     if (!conf->dc_enable_parking_brake)
@@ -78,10 +94,10 @@ static void apply_parking_brake(float dutycycle, bool engaged)
         return;
     }
 
-    // If parking brake is engaged, make sure no current flows (No current -> Parking brake applied)
-    if (!engaged)
+    // If parking brake output is not engaged, make sure no current flows
+    if (!output_engaged)
     {
-        TIM_SelectOCxM(TIM1, TIM_Channel_2, TIM_ForcedAction_InActive); // Could also be TIM_OCMode_Inactive, but I don't think so
+        TIM_SelectOCxM(TIM1, TIM_Channel_2, TIM_ForcedAction_InActive); 
         TIM_CCxCmd(TIM1, TIM_Channel_2, TIM_CCx_Enable);
         TIM_CCxNCmd(TIM1, TIM_Channel_2, TIM_CCxN_Enable);
         TIM_GenerateEvent(TIM1, TIM_EventSource_COM);
@@ -100,6 +116,9 @@ static void apply_parking_brake(float dutycycle, bool engaged)
     is_ccr_called += 1;
 }
 
+/**
+ * Stop the PWM output of the brake
+ */
 void mcpwm_dc_parking_brake_stop_pwm(void)
 {
     TIM_SelectOCxM(TIM1, TIM_Channel_2, TIM_ForcedAction_InActive);
@@ -109,29 +128,42 @@ void mcpwm_dc_parking_brake_stop_pwm(void)
 }
 
 /**
- * Enables the parking brake if dutyCycle is correct. Bigger than 0 will apply the parking brake
- * Smaller will disengage it.
+ * Enables the parking brake output. The real switching happens in the control loop
  *
- * @param dutycycle
- * The parameter on basis of which it is decided to engage/disengage the parking brake
+ * @param output_engaged whether to engage the output
  */
-void mcpwm_dc_set_parking_brake(bool engaged)
+void mcpwm_dc_set_parking_brake(bool output_engaged)
 {
-    parking_brake_output_engaged = engaged;
+    parking_brake_output_engaged = output_engaged;
 }
 
+/**
+ * Get whether the parking brake is enabled in config
+ */
 bool mcpwm_dc_is_parking_brake_enabled(void)
 {
     return conf->dc_enable_parking_brake;
 }
+
+/**
+ * Get whether the parking brake is engaged currently
+ */
 bool mcpwm_dc_is_parking_brake_engaged(void)
 {
     return parking_brake_output_engaged;
 }
+
+/**
+ * Get the currently set duty cycle
+ */
 float mcpwm_dc_get_parking_brake_duty(void)
 {
     return dutycycle_parking_brake_now;
 }
+
+/**
+ * Some getters to debug the parking brake module
+ */
 uint16_t mcpwm_dc_get_ccr_called(void)
 {
     return is_ccr_called;
